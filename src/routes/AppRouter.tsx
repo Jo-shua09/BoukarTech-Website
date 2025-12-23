b0c77b15796aca414b84b84ddb7061dba79e3328
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-12-22 22:31:38 -0800

    `Added ScrollToTop component to AppRouter`

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -9,6 +9,7 @@ import Career from "@/pages/Career";
 import Contact from "@/pages/Contact";
 import NotFound from "@/pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const AppRouter = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
