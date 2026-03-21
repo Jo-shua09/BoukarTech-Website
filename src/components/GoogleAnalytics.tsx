89efeaf76569c1af6b88d72c0782a586278bb656
Jo-shua09 <joshuaonyeka039@gmail.com>
2026-03-21 13:34:21 -0700

    `Update Google Analytics tracking ID in index.html and GoogleAnalytics.tsx`

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -13,7 +13,7 @@ const GoogleAnalytics = () => {
   useEffect(() => {
     // Track page views when the route changes
     if (typeof window.gtag === "function") {
-      window.gtag("config", "G-4MN1D7DN01", {
+      window.gtag("config", "G-Y30LZHK8N8", {
         page_path: location.pathname + location.search,
       });
     }
